28770bf3066367313b7f31c8ef7dd51b73dd7414
saba qatamadze <sabagamer098@gmail.com>
2026-09-10 17:16:14 +0400

    Update Product.jsx

--- Shopify-Project-main/src/Pages/Product/Product.jsx
+++ Shopify-Project-main/src/Pages/Product/Product.jsx
@@ -2,8 +2,8 @@ import { getSingleProduct } from "@/helper/api";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Product.css";
-import { AuthContext } from "@/../../contexts/AuthContext";
-import { IsAuthModalOpenContext } from "@/../../contexts/AuthModalContext";
+import { AuthContext } from "@/contexts/AuthContext";
+import { IsAuthModalOpenContext } from "@/contexts/AuthModalContext";
 
 const Product = () => {
   const { product_id } = useParams();
